2ecf7d0425337f89c951701e14d4a4fe4a9350e7
Jose Vega <josevega234@gmail.com>
2023-04-12 21:48:39 +0200

    visual multi

--- plugins/user.lua
+++ plugins/user.lua
@@ -43,6 +43,19 @@ return {
       vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
     end,
   },
+  {
+    "mg979/vim-visual-multi",
+    keys = {
+      "<C-n>",
+      "<C-Up>",
+      "<C-Down>",
+      "<S-Up>",
+      "<S-Down>",
+      "<S-Left>",
+      "<S-Right>",
+    },
+    config = function() vim.g.VM_leader = ";" end,
+  },
   -- LSP
   {
     "weilbith/nvim-code-action-menu",
