cdfad6d78364aeaccce3848d08fcce0c398ce765
Jose Vega <jevf@gft.com>
2023-07-23 01:22:24 +0200

    autocmds

--- config/autocmds.lua
+++ config/autocmds.lua
@@ -18,3 +18,29 @@ vim.api.nvim_create_autocmd("VimLeave", {
   pattern = "*",
   callback = function() vim.fn.jobstart { "autocomp", vim.fn.expand "%:p", "stop" } end,
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp_attach_auto_diag", { clear = true }),
+  callback = function(args)
+    -- the buffer where the lsp attached
+    ---@type number
+    local buffer = args.buf
+
+    -- create the autocmd to show diagnostics
+    vim.api.nvim_create_autocmd("CursorHold", {
+      group = vim.api.nvim_create_augroup("_auto_diag", { clear = true }),
+      buffer = buffer,
+      callback = function()
+        local opts = {
+          focusable = false,
+          close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+          border = "rounded",
+          source = "always",
+          prefix = " ",
+          scope = "cursor",
+        }
+        vim.diagnostic.open_float(nil, opts)
+      end,
+    })
+  end,
+})
