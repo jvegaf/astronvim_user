78adc7f34822685cbb588017513eae15d990b402
Jose Vega <jvega@tulotero.com>
2024-03-04 23:46:08 +0100

    community

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,7 +7,7 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
   { import = "astrocommunity.completion.cmp-cmdline" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.editing-support.neogen" },
   { import = "astrocommunity.editing-support.nvim-regexplainer" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
@@ -20,45 +20,18 @@ return {
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.mini-ai" },
   { import = "astrocommunity.motion.mini-move" },
-  { import = "astrocommunity.programming-language-support.rest-nvim" },
+  -- { import = "astrocommunity.programming-language-support.rest-nvim" },
   { import = "astrocommunity.project.nvim-spectre" },
   { import = "astrocommunity.project.project-nvim" },
   { import = "astrocommunity.register.nvim-neoclip-lua" },
   { import = "astrocommunity.syntax.hlargs-nvim" },
-  { import = "astrocommunity.utility.neodim" },
+  -- { import = "astrocommunity.utility.neodim" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.vue" },
-  { import = "astrocommunity.pack.java" },
-  { import = "astrocommunity.pack.dart" },
   { import = "astrocommunity.pack.kotlin" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.angular" },
   { import = "astrocommunity.pack.html-css" },
-  -- {
-  --   "mfussenegger/nvim-jdtls",
-  --   opts = {
-  --     settings = {
-  --       java = {
-  --         configuration = {
-  --           runtimes = {
-  --             {
-  --               name = "JavaSE-11",
-  --               path = "/home/jvegaf/.sdkman/candidates/java/11.0.19-tem",
-  --             },
-  --             {
-  --               name = "JavaSE-17",
-  --               path = "/home/jvegaf/.sdkman/candidates/java/17.0.8.fx-zulu",
-  --             },
-  --             {
-  --               name = "JavaSE-21",
-  --               path = "/home/jvegaf/.sdkman/candidates/java/21-tem",
-  --             },
-  --           },
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
 }
