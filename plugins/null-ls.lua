10eec1edb21b8174440bdc60199d23b62770605f
Jose Vega <jvega@tulotero.com>
2024-03-04 23:49:57 +0100

    dart fck

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -12,7 +12,6 @@ return {
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.prettierd,
       null_ls.builtins.formatting.jq,
-      null_ls.builtins.formatting.dart_format,
     }
     return config -- return final config table
   end,
